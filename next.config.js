362fd534ed31ff335b638bf98d742b0c28bcfa59
revell29 <diraapsya0@gmail.com>
2024-03-01 07:47:13 +0700

    fix: build

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   eslint: {
     dirs: ['src'],
+    ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
+  swcMinify: true,
+
   images: {
     domains: ['jb-assets-alpha.sg-sin1.upcloudobjects.com'],
   },
